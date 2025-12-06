55e843841bd568bd37a6e18b2c717a2ac425392f
Nguyễn Đức Huy <126212666+Huy22032002@users.noreply.github.com>
2025-12-06 13:17:50 +0700

    fix: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: process.env.VITE_BAE_PATH || "/Frontend_HTeam_Ecommerce",
-=======
   server: {
     port: 5173,
   },
->>>>>>> dev
 });
